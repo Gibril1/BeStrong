10ed1cbecdb610e4b61048f774165f09428bdb30
Gibril <gibrilissakaiddrisu@gmail.com>
2024-02-06 10:39:03 +0000

    api done

---
+++ api.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from service import OpenAIService
+
+
+class PromptSchema(BaseModel):
+    prompt: str
+
+openai_service = OpenAIService()
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {
+        "success": True,
+        "data": "The API is working on this port"
+    }
+
+
+@app.post('/prompt')
+async def prompt(prompt:PromptSchema):
+    if not prompt.prompt:
+        return {
+            "success": False,
+            "message": "Please enter a prompt"
+        }
+    prompt_response = await openai_service.prompt(prompt.prompt)
+    return {
+        "success": True,
+        "data": prompt_response
+    }
+
